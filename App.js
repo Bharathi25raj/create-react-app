03e10d62cd4345d63e6367d7e28a190d76be7d86
Bharathi25raj <bharathi25raj@gmail.com>
2024-07-11 17:24:56 +0530

    episode-03

--- App.js
+++ App.js
@@ -13,6 +13,38 @@ const parent = React.createElement("div", {id: "parent"}, [
     ]),
 ])
 
+const heading = React.createElement("h1", {id:"heading"}, "Namaste React");
+console.log(heading);
+
+const jsxHeading = <h1 id="heading" className="head" tabIndex="1">Namaste React using JSX</h1>;
+console.log(jsxHeading);
+
+const elem = <h1>React element</h1>;
+
+const Title = () => (
+    <h1 className="title" tabIndex="5">Namaste React using JSX {elem}</h1>
+)
+
+const HeadingComponent = () => {
+    return <h1 id="heading">Namaste React Functional Component </h1>
+}
+
+const HeadingComponent2 = () => (
+    <div id="container">
+        {/* <Title /> */}
+        {Title()}
+        <h1 className="heading">Namaste React from Functional Component</h1>
+    </div>
+)
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+// root.render(parent);
+// root.render(heading);
+// root.render(jsxHeading);
+
+root.render(<HeadingComponent2 />);
+// root.render(elem);
+// root.render(Title());
